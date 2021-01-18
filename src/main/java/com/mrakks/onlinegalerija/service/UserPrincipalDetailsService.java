26f47513fce1f0b5643cec3a2f9980e65f2bad1b
mrakic96 <matej.rakic96@gmail.com>
2021-01-18 21:09:26 +0100

    Create UserPrincipal and UserPrincipalDetailsService

---
+++ src/main/java/com/mrakks/onlinegalerija/service/UserPrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.mrakks.onlinegalerija.service;
+
+import com.mrakks.onlinegalerija.model.User;
+import com.mrakks.onlinegalerija.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
